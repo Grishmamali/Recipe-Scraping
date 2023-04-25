af0af8b20748e5d4a2c34e5819e83049d6d2e0d0
priya <priyankagajbhiye110415@gmail.com>
2023-04-24 22:48:38 -0400

    pushing code

--- src/test/java/scrapedata/Baseclass.java
+++ src/test/java/scrapedata/Baseclass.java
@@ -1,11 +1,15 @@
 package scrapedata;
+import java.time.Duration;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import utility.Readconfig;
@@ -23,28 +27,32 @@ public class Baseclass {
 	
 
 	@Parameters("browser")
-
 	@BeforeClass
-	public void setup(String browser) {
+	public void setup(@Optional String browser) {
 
 		try {
 			logger = Logger.getLogger("webScrapping");
 			PropertyConfigurator.configure("Log4j.properties");
 
-			if(browser.equals("chrome"))
-			{
-				System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
-				driver=new ChromeDriver();
-			}
-			else if(browser.equals("firefox"))
+			if("firefox".equalsIgnoreCase(browser))
 			{
 				System.setProperty("webdriver.gecko.driver",readconfig.getFirefoxPath());
 				driver = new FirefoxDriver();
 			}
+			else 
+			{
+				System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
+				driver=new ChromeDriver();
+			}
 			logger.info("********************welcome*********************");
-
+            
+			ChromeOptions co = new ChromeOptions();
+			co.setAcceptInsecureCerts(true);
 			driver.get(baseURL);
-			driver.manage().window().maximize();}
+			driver.manage().window().maximize();
+			
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(90));
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));}
 
 			catch (Exception e) {
 				logger.error("Error in Test:",e);
@@ -55,4 +63,8 @@ public class Baseclass {
 			}
 
 		}
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 }
---
+++ src/test/java/utility/ingredients.java
@@ -0,0 +1,39 @@
+package utility;
+
+public class ingredients {
+	
+	public enum HypertensionEliminates{
+		
+		salty_food("Salty food") , 
+		chips("Chips"),
+		pretzels("Pretzels"), 
+		crackers("crackers"),
+		coffee("coffee"),
+		tea("tea"),
+		softdrinks("softdrinks"),
+		alcohol("alcohol"),
+		frozen_food("frozen food"),
+		pickles("pickles"),
+		canned_food("canned food"),
+		processedfood("processed food"),
+		friedfood("fried food"), 
+		Sauces("Sauces"),
+		mayonnaise("mayonnaise"),
+		whiterice("white rice"),
+		whitebread("white bread");
+
+		HypertensionEliminates(String string) {
+			
+		}
+			
+	}
+	
+	
+	
+	
+	
+		
+		
+	
+
+}
